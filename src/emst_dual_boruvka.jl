dac69c8cc0a8518e824b928cc7ce49463a60a0c2
Krzysztof Nowak <krzysztof9nowak@gmail.com>
2022-04-09 06:37:34 +0100

    Refactoring

--- src/emst_dual_boruvka.jl
+++ src/emst_dual_boruvka.jl
@@ -23,8 +23,6 @@ function to_matrix(ee::Vector{Edge})
 return m
 end
 
-
-
 """
   DTBNode
 
@@ -40,7 +38,6 @@ mutable struct DTBNode
     # data / indidces of this kd tree node
     data::Array{Float64,2}
     subset::Array{Int64}
-    # splits::Array{DTBSplit,1}
 
     # kd tree node bounds
     lb::Array{Float64,1}
@@ -52,8 +49,6 @@ mutable struct DTBNode
     left::DTBNode
     right::DTBNode
 
-    # DTBNode(data::Array{Float64,2}) = (x = new(data); x.left=x; x.right=x)
-    # function DTBNode(data::Array{Float64,2},subset::Array{Int64},splits::Array{DTBSplit,1},dQ::Float64)
     function DTBNode(id::Int64, data::Array{Float64,2}, subset::Array{Int64}, box_lb::Array{Float64,1}, box_ub::Array{Float64,1}, dQ::Float64)
         n = new(id, data, subset, box_lb, box_ub, dQ)
         n.left = n
@@ -93,7 +88,6 @@ computes splits of KD tree. nmin indicates the node size where we stop
 splitting nodes.
 """
 function kdtree_split!(node::DTBNode, nmin::Int64)
-
     # ensure that parent node is in the tree.
     # t[node.id] = node
 
@@ -109,10 +103,10 @@ function kdtree_split!(node::DTBNode, nmin::Int64)
     bx   = node.data[ds,:] .<= vs
 
     range_a = node.subset[bx]
-    range_b = node.subset[.~bx]
+    range_b = node.subset[.!bx]
 
     data_a = node.data[:,bx]
-    data_b = node.data[:,.~bx]
+    data_b = node.data[:,.!bx]
 
     box_lb_a = copy(node.lb)
     box_ub_a = copy(node.ub)
@@ -173,105 +167,72 @@ function dtb(q::DTBNode, e::IntDisjointSets)
             C_dcq[ri] = Inf
         end
 
-        # compute..
-        # fcn(q,q,e,C_dcq,C_e , hashmap_QvsR )
-        fcn(q, q, e, C_dcq, C_e)
+        find_component_neighbors(q, q, e, C_dcq, C_e)
 
         # and now add the edges..
         for ne::Edge in values(C_e)
             union!(e, ne.a, ne.b)
             push!(edges, ne)
-    end
+        end
     end
     return edges
 end
 
 
 """
-  fcn(q::DTBNode,r::DTBNode,e::IntDisjointSets , C_dcq::Dict{Int64,Float64} , C_e::Dict{Int64,MyE} )
 Find Component Neighbors
 
 Components are identified by the root of the component
 C_dcq : component distances to candidate edges
 C_e   : component candidate edges (comp i candidate edge is C_e[:,i])
 """
-function fcn(q::DTBNode, r::DTBNode, e::IntDisjointSets, C_dcq::Dict{Int64,Float64}, C_e::Dict{Int64,Edge})
+function find_component_neighbors(Q::DTBNode, R::DTBNode, forest::IntDisjointSets, C_dcq::Dict{Int64,Float64}, C_e::Dict{Int64,Edge})
     # check all in same component
     onecomp::Bool  = true
-    joined         = [q.subset;r.subset]
-    # println(joined)
+    joined         = [Q.subset;R.subset]
     for ji in joined
-        if (~in_same_set(e, joined[1], ji))
+        if !in_same_set(forest, joined[1], ji)
             onecomp = false
             break
+        end
     end
-    end
-    if (onecomp)
+    if onecomp
         return
     end
 
-    # check d(Q,R) > d(Q)
-    dqr = computeDQR(q, r)
-    if ( dqr > q.dQ)
+    if distance(Q, R) > Q.dQ
         return
     end
 
     # check if R and Q in a leaf node
-    if ( is_leaf(q) && is_leaf(r) )
-        # n_dQ = -Inf
-        n_dQ::Float64 = q.dQ
+    if ( is_leaf(Q) && is_leaf(R) )
+        n_dQ::Float64 = Q.dQ
 
-        all_d_qr = Distances.pairwise(Euclidean(), q.data, r.data, dims=2)
-
-        # --> hashing these values does not really speed up things..
-        #     and it uses tons of memory, such that fancier handling of
-        #     hash map entries would be required
-        # if(!haskey(hashmap_QvsR,(q,r)))
-        #    if(!haskey(hashmap_QvsR,(r,q)))
-        #        all_d_qr = Distances.pairwise(Euclidean(),q.data,r.data)
-        #        #hashmap_QvsR[(q,r)] = all_d_qr
-        #    else
-        #        all_d_qr = transpose(hashmap_QvsR[(r,q)])
-        #    end
-        #    hashmap_QvsR[(r,q)] = transpose(all_d_qr)
-        # else
-        #    all_d_qr = hashmap_QvsR[(q,r)]
-        #    #print('.')
-        # end
-
-        for iq = 1:size(q.subset, 1)
-            for ir = 1:size(r.subset, 1)
-                qq = q.subset[iq]
-                rr = r.subset[ir]
-                if ( in_same_set(e, qq, rr) )
-                    continue
-                end
-
-                cq = find_root(e, rr) # compoment of q
-
-                # check distance:
-                # dist_qr::Float64 = sqrt( sum( (q.data[:,iq]-r.data[:,ir]).^2 ) )
-                dist_qr = all_d_qr[iq,ir]
-
-                if ( dist_qr < C_dcq[ cq ] )
-                    C_dcq[ cq ] = dist_qr
-                    C_e[ cq ]   = Edge(qq, rr) # (qq,rr)
-                    # and dQ !
-                    n_dQ = max(n_dQ, dist_qr)
-                    # n_dQ = dist_qr
+        pairwise_d = Distances.pairwise(Euclidean(), Q.data, R.data, dims=2)
+        for (iq, qq) in enumerate(Q.subset), (ir, rr) in enumerate(R.subset)
+            if in_same_set(forest, qq, rr)
+                continue
             end
+
+            cq = find_root(forest, rr) # tree of q
+
+            dist_qr = pairwise_d[iq, ir]
+            if dist_qr < C_dcq[ cq ]
+                C_dcq[ cq ] = dist_qr
+                C_e[ cq ]   = Edge(qq, rr)
+                n_dQ = max(n_dQ, dist_qr)
             end
         end
-        q.dQ = n_dQ
+        Q.dQ = n_dQ
 
         return
     end
 
-    fcn(q.left, r.left, e,  C_dcq, C_e)
-    fcn(q.right, r.left, e,  C_dcq, C_e)
-    fcn(q.left, r.right, e,  C_dcq, C_e)
-    fcn(q.right, r.right, e,  C_dcq, C_e)
-    q.dQ = max(q.left.dQ, q.right.dQ)
+    find_component_neighbors(Q.left, R.left, forest,  C_dcq, C_e)
+    find_component_neighbors(Q.right, R.left, forest,  C_dcq, C_e)
+    find_component_neighbors(Q.left, R.right, forest,  C_dcq, C_e)
+    find_component_neighbors(Q.right, R.right, forest,  C_dcq, C_e)
+    Q.dQ = max(Q.left.dQ, Q.right.dQ)
 end
 
 
@@ -286,7 +247,7 @@ end
 """
 compute min dist. between bounding boxes, i.e. between rectangular boxes Q/R with
 """
-function computeDQR(q::DTBNode, r::DTBNode)
+function distance(q::DTBNode, r::DTBNode)
     rdists = [intervals_distance(xl, xu, yl, yu) for (xl, xu, yl, yu) in zip(q.lb, q.ub, r.lb, r.ub)]
     return sqrt(sum(rdists.^2))
 end
